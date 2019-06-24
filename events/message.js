b6305b5ef0f5fbe39d28cde3f5e264eb77fe0fdf
Harry Collet <harrison.collet@wsu.edu>
2019-06-23 22:08:57 -0700

    Added additional files to make directory neater and implemented two commands, !list-games and !shared-games

--- events/message.js
+++ events/message.js
@@ -7,7 +7,10 @@ module.exports = (Bot, msg) => {
     if(msg.content === 'hi'){
         msg.reply('Boo!');
     }
-    if(msg.content.startsWith("!shared-games")){
-        events.sharedGames(Bot, msg);
+    if(msg.content.startsWith("!shared-games ")){
+        events.sharedGames(Bot, msg, msg.content.replace("!shared-games ", ""));
+    }
+    if(msg.content.startsWith('!list-games ')){
+        events.listGames(Bot, msg, msg.content.replace("!list-games ", ""));
     }
 }
